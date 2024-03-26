8506e3928cb1aeb621290eda34533c38bfb9e660
swamyvitasoft <swamy.vitasoft@gmail.com>
2024-03-26 12:15:55 +0530

    item controller

---
+++ common/itemsmodel.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema({
+    itemname: {
+        type: String,
+        required: true
+    }
+},
+    { timestamps: true }
+);
+
+const Itemmodel = mongoose.model('item', itemSchema);
+
+export default Itemmodel
---
+++ item/index.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import {
+    addItems,
+    getAllitems,
+    getItemwithId,
+    itemUpdatebyId,
+    itemDeletebyId
+} from "../item/controller.js"
+import verifyToken from "../jwt/verifytoken.js";
+
+const router = new Router()
+
+router.post('/additem', addItems);
+
+router.get('/getitems', verifyToken, getAllitems);
+
+router.get('/getItembyId/:id', verifyToken, getItemwithId);
+
+router.put('/upadate/:id', verifyToken, itemUpdatebyId);
+
+router.delete('/delete/:id', verifyToken, itemDeletebyId);
+
+
+export default router
