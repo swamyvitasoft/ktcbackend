8bbcd58a5f6afb21faa31bd9f7001d5733f539cb
swamyvitasoft <swamy.vitasoft@gmail.com>
2024-03-28 21:32:40 +0530

    model changes

--- common/itemsmodel.js
+++ common/itemsmodel.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
-const itemSchema = new mongoose.Schema({
+const itemSchema = new mongoose.Schema(
+  {
     itemname: {
-        type: String,
-        required: true
-    }
-},
-    { timestamps: true }
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
-const Itemmodel = mongoose.model('item', itemSchema);
+const Itemmodel = mongoose.model("item", itemSchema);
 
-export default Itemmodel
+export default Itemmodel;
--- common/salesmodel.js
+++ common/salesmodel.js
@@ -1,47 +1,52 @@
+import { ObjectId } from "mongodb";
 import mongoose from "mongoose";
 
-const saleSchema = new mongoose.Schema({
-   
-    fullname:{
-        type:String,
-        required:true 
-     },   
-     address:{
-        type:String,
-        required:true 
-     },
-     mobileno:{
-        type:String,
-        required:true 
-     },
-     particulars:{
-        type:String,
-        required:true 
-     },
-     items:{
-        type:String,
-        required:true 
-     },
-     imei:{
-        type:String,
-        required:true 
-     },
-     estimatedamount:{
-        type:String,
-        required:true 
-     },
-     advanceamount:{
-        type:String,
-        required:true 
-     },
-     balaceamount:{
-        type:String,
-        required:true 
-     }
-},
-{timestamps:true}
+const saleSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    mobileno: {
+      type: String,
+      required: true,
+    },
+    particulars: {
+      type: String,
+      required: true,
+    },
+    itemId: {
+      type: ObjectId,
+      required: true,
+    },
+    imei: {
+      type: String,
+      required: true,
+    },
+    estimatedamount: {
+      type: String,
+      required: true,
+    },
+    advanceamount: {
+      type: String,
+      required: true,
+    },
+    balaceamount: {
+      type: String,
+      required: true,
+    },
+    note: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
-const Salemodel = mongoose.model('sale',saleSchema);
+const Salemodel = mongoose.model("sale", saleSchema);
 
-export default Salemodel
+export default Salemodel;
